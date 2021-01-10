6fd2986652932f2be134745873978c9ab189d556
EwenQuim <ewen.quimerch@gmail.com>
2021-01-10 23:12:09 +0100

    feat: 3 pages

--- app/src/main/java/com/amethysts/recettes/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/amethysts/recettes/ui/main/SectionsPagerAdapter.kt
@@ -7,16 +7,17 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.amethysts.recettes.R
 
 private val TAB_TITLES = arrayOf(
-        R.string.tab_text_1,
-        R.string.tab_text_2
+    R.string.tab_text_1,
+    R.string.tab_text_2,
+    R.string.tab_text_3
 )
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
  * one of the sections/tabs/pages.
  */
-class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
-    : FragmentPagerAdapter(fm) {
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
@@ -24,12 +25,12 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
         return PlaceholderFragment.newInstance(position + 1)
     }
 
-    override fun getPageTitle(position: Int): CharSequence? {
+    override fun getPageTitle(position: Int): CharSequence {
         return context.resources.getString(TAB_TITLES[position])
     }
 
     override fun getCount(): Int {
         // Show 2 total pages.
-        return 2
+        return 3
     }
 }
